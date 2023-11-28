8e48de515c125d42fc8abf701fa71a5cf60f9d88
Sharell Bryant <sharell.bryant@gmail.com>
2023-11-28 13:34:42 -0500

    Update neopixel stick code to display red

--- neopixel_stick/neopixel_stick.ino
+++ neopixel_stick/neopixel_stick.ino
@@ -1,59 +1,35 @@
+// Adapted from: 
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // Released under the GPLv3 license to match the rest of the
 // Adafruit NeoPixel library
 
 #include <Adafruit_NeoPixel.h>
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        2 // On Trinket or Gemma, suggest changing this to 1
-#define PIN2      3
+#define LED_PIN        2
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 8 // Popular NeoPixel ring size
+#define LED_COUNT 8 // Popular NeoPixel stick size
 
 // When setting up the NeoPixel library, we tell it how many pixels,
-// and which pin to use to send signals. Note that for older NeoPixel
-// strips you might need to change the third parameter -- see the
-// strandtest example for more information on possible values.
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels2(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+// and which pin to use to send signals
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGBW + NEO_KHZ800);
 
 #define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
 void setup() {
-
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels2.begin();
+  strip.begin(); // INITIALIZE NeoPixel strip object
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  strip.clear(); // Set all pixel colors to 'off'
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+  for(int i=0; i<LED_COUNT; i++) { // For each pixel...
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    strip.setPixelColor(i, strip.Color(0, 255, 0)); // red
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVAL); // Pause before next pass through loop
-  }
-
-
-  pixels2.clear(); // Set all pixel colors to 'off'
-
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels2.setPixelColor(i, pixels2.Color(0, 150, 0));
-
-    pixels2.show();   // Send the updated pixel colors to the hardware.
+    strip.show();   // Send the updated pixel colors to the hardware.
 
     delay(DELAYVAL); // Pause before next pass through loop
   }
